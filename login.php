9ade2c27b8b2c6353277663778fee260207fab00
Al Capitan <alexandre.boyer29@gmail.com>
2022-08-25 16:28:39 +0200

    feat : Ajout CSS pour signin et login

--- login.php
+++ login.php
@@ -1,14 +1,14 @@
 <?php  
 error_reporting(0);
-//Connexion a la bdd 
+// Connexion à la base de données
 require_once 'bdd/database.php';
 // Création de l'instance de la classe
 $database = new Database();
 
-//Démarage de la session (a mettre dans toutes les pages.)
+// Démarrage de la session (à mettre dans toutes les pages)
 session_start();
 
-//Récuperation de la function est_connecter (Boolean)
+// Récupération de la function est_connecter (Boolean)
 require_once 'function/auth.php';
 
 if(!est_connecter()){
@@ -20,55 +20,65 @@ if(!est_connecter()){
     <p>Vous êtes connecter.</p>
     <?php
 }
-
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Formulaire de connexion de Server Exchanges. ">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="index,follow">
+    <link rel="canonical" href="https://alcapitan.github.io/atui">
+    <meta name="theme-color" content="#e63300">
     <title>Connexion</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Red+Hat+Display&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/alcapitan/atui@dev/atui/kernel/main.css">
+    <link rel="stylesheet" type="text/css" href="forms.css">
+    <link rel="icon" type="img/png" href="https://cdn.jsdelivr.net/gh/alcapitan/atui@dev/patch/icons/logo.png">
 </head>
 <body>
-    <h1>Page de connexion</h1>
-    <form method="POST" >
-        <input type="email" placeholder="Email" name="email" required> <br> <br>
-        <input type="password" placeholder="Mot de passe" name="password" required> <br> <br>
-        <?php echo $_GET['status'] ?>
-
-        <input type="submit" name="send">
-
-        <a href="signin.php">
-            Je n'ai pas de compte
-        </a>
-    </form>
-<?php 
-    if(!empty($_POST['send'])){
-            // htmlentities est une function en php pour éviter les fails du code html.
-            $atuiServerExchanges_Sendemail = htmlentities($_POST['email']);
-            $atuiServerExchanges_Sendpassword = htmlentities($_POST['password']);
-            //BINARY est en sql pour qui prend compte des majuscule est minuscule.
-            $query = "SELECT * FROM utilisateur WHERE BINARY email='".$atuiServerExchanges_Sendemail."'";
-            $data = $database->read($query);
-            foreach ($data as $dataV2 ) {
-                $atuiServerExchanges_SendpasswordHash = $dataV2['password'];
-            }
-            if(!empty($data[0])){
-                if(password_verify($atuiServerExchanges_Sendpassword, $atuiServerExchanges_SendpasswordHash)){
-                    $_SESSION['connecte'] = 1;
-                    header("Location: confirmer/panel.php");
-                }else{
-                    $atuiServerExchanges_Error = "Mot de passe ou email incorect";
-                    ?>
-                        <script>
-                            location.replace("signin.php?status=<?=$atuiServerExchanges_Error?>")
-                        </script>
-                    <?php
-                }
-            }
-            }
-        ?>
+    <section>
+        <article>
+            <img src="https://cdn.jsdelivr.net/gh/alcapitan/atui@dev/patch/icons/logo.png" alt="Logo d'ATUI">
+            <h1>Connexion</h1>
+            <form method="POST">
+                <input type="email" placeholder="Email" name="email" required>
+                <input type="password" placeholder="Mot de passe" name="password" required>
+                <?php echo $_GET['status'] ?>
+                <input type="submit" name="send" value="Se connecter">
+                <?php 
+                if(!empty($_POST['send'])){
+                    // htmlentities est une function en php pour éviter les erreurs HTML.
+                    $atuiServerExchanges_Sendemail = htmlentities($_POST['email']);
+                    $atuiServerExchanges_Sendpassword = htmlentities($_POST['password']);
+                    // BINARY est une fonction en sql qui ne prend pas en compte les majuscules et minuscules.
+                    $query = "SELECT * FROM utilisateur WHERE BINARY email='".$atuiServerExchanges_Sendemail."'";
+                    $data = $database->read($query);
+                    foreach ($data as $dataV2 ) {
+                        $atuiServerExchanges_SendpasswordHash = $dataV2['password'];
+                    }
+                    if(!empty($data[0])){
+                        if(password_verify($atuiServerExchanges_Sendpassword, $atuiServerExchanges_SendpasswordHash)){
+                            $_SESSION['connecte'] = 1;
+                            header("Location: confirmer/panel.php");
+                        }else{
+                            $atuiServerExchanges_Error = "Email et mot de passe incorrect";
+                            ?>
+                                <script>
+                                    location.replace("signin.php?status=<?=$atuiServerExchanges_Error?>")
+                                </script>
+                            <?php
+                        }
+                    }
+                    }
+                ?>
+            </form>
+            <a href="signin.html">Je n'ai pas de compte</a>
+        </article>
+    </section>
+    <script src="https://cdn.jsdelivr.net/gh/alcapitan/atui@dev/atui/kernel/main.js"></script>
 </body>
 </html>
--- signin.php
+++ signin.php
@@ -1,60 +1,72 @@
 <?php
-//On dégage tous les erreurs inutiles. 
+// On supprime toutes les erreurs 
 error_reporting(0);
 
-// Connexion a la bdd
+// Connexion à la base de données
 require_once 'bdd/database.php';
 $database = new Database();
-
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Formulaire d'inscription de Server Exchanges. ">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="index,follow">
+    <link rel="canonical" href="https://alcapitan.github.io/atui">
+    <meta name="theme-color" content="#e63300">
     <title>Inscription</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Red+Hat+Display&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/alcapitan/atui@dev/atui/kernel/main.css">
+    <link rel="stylesheet" type="text/css" href="forms.css">
+    <link rel="icon" type="img/png" href="https://cdn.jsdelivr.net/gh/alcapitan/atui@dev/patch/icons/logo.png">
 </head>
 <body>
-    <h1>Page d'inscription</h1>
-    <form method="POST" >
-        <input type="email" placeholder="Email" name="email" required> <br> <br>
-        <input type="password" placeholder="Mot de passe" name="password" required> <br> <br>
-        <?php echo $_GET['status']; ?>
-        <input type="submit" name="send" value="S'inscrire">
-    </form>
+    <section>
+        <article>
+            <img src="https://cdn.jsdelivr.net/gh/alcapitan/atui@dev/patch/icons/logo.png" alt="Logo d'ATUI">
+            <h1>Inscription</h1>
+            <form method="POST">
+                <input type="email" placeholder="Email" name="email" required>
+                <input type="password" placeholder="Mot de passe" name="password" required>
+                <?php echo $_GET['status']; ?>
+                <input type="submit" name="send" value="S'inscrire">
+            </form>
+            <a href="login.html">J'ai déjà un compte</a>
+        </article>
+    </section>
     <?php 
-
-    //On regarder si le formulaire est vide ou pas.
+    // On regarde si le formulaire est vide ou pas.
     if(!empty($_POST['send'])){
-        // On évite les failles html
+        // On évite les erreurs HTML
         $atuiServerExchanges_Sendemail = htmlentities($_POST['email']);
 
         $atuiServerExchanges_Sendpassword = htmlentities($_POST['password']);
         $atuiServerExchanges_SendpasswordHash = password_hash($atuiServerExchanges_Sendpassword, PASSWORD_DEFAULT);
 
-        // On regarder si l'utilisateur existe ou pas.
+        // On regarde si l'utilisateur est déjà enregistré.
         $atuiServerExchanges_query = "SELECT * FROM utilisateur WHERE BINARY email='".$atuiServerExchanges_Sendemail."'";
         $data = $database->read($atuiServerExchanges_query);
 
         if(!empty($data[0])){
-                ?>
-                    <script>
-                        location.replace("signin.php?status=Email déjà enregistrer.")
-                    </script>
-                <?php
-
+            ?>
+                <script>
+                    location.replace("signin.php?status=Email déjà enregistré.")
+                </script>
+            <?php
         }else{
             $atuiServerExchanges_query2 = "INSERT INTO `utilisateur`(`email`, `password`) VALUES ('$atuiServerExchanges_Sendemail','$atuiServerExchanges_SendpasswordHash')";
             $data2 = $database->read($atuiServerExchanges_query2);
 
-            echo "Utilisateur bien rajouter."
+            echo "Utilisateur inscrit."
             ?>
-                <a href="login.php">Revenir a la page de connexion.</a>
+                <a href="login.php">Aller à la page de connexion.</a>
             <?php
         }
     }
     ?>
+    <script src="https://cdn.jsdelivr.net/gh/alcapitan/atui@dev/atui/kernel/main.js"></script>
 </body>
 </html>
